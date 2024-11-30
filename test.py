7cc3fe22c8d2c1246612640346941a0101573e56
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-11-29 16:42:19 -0800

    Sort note sequences into dictionary

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ def process_track(track):
     events = []
     absolute_time = 0
     for msg in track:
-        absolute_time += msg.time  # Accumulate delta time to get absolute time
-        if msg.type in ["note_on", "note_off"]:
+        absolute_time += msg.time # Accumulate delta time to get absolute time
+        if msg.type in ["note_on"] and msg.velocity > 0: # TODO add support for note off
             event = {
                 "time": absolute_time,
                 "note": msg.note,
@@ -29,7 +29,6 @@ def midi_note_to_letter(note_number):
     note = note_names[note_number % 12]
     return f"{note}{octave}"
 
-
 # Process each track in the MIDI file
 right_hand_events = process_track(midi_file.tracks[0])
 left_hand_events = process_track(midi_file.tracks[1])
@@ -37,14 +36,14 @@ left_hand_events = process_track(midi_file.tracks[1])
 # Merge and align events by time
 all_events = sorted(right_hand_events + left_hand_events, key=lambda x: x["time"])
 
-# Output aligned notes
-print("Aligned Notes (time, right hand, left hand):")
+# Create a dictionary of notes at each time
+noteSequence = {}
 for event in all_events:
-    if event in right_hand_events:
-        print(
-            f"{event['time']}: Right hand - Note {midi_note_to_letter(event['note'])} {'On' if event['velocity'] > 0 else 'Off'}"
-        )
-    else:
-        print(
-            f"{event['time']}: Left hand - Note {midi_note_to_letter(event['note'])} {'On' if event['velocity'] > 0 else 'Off'}"
-        )
+    if event['time'] not in noteSequence:
+        noteSequence[event['time']] = []
+    noteSequence[event['time']].append(event['note'])
+
+# Print noteSequence with keys in order and their corresponding values
+for time in sorted(noteSequence.keys()):
+    notes = [midi_note_to_letter(note) for note in noteSequence[time]]
+    print(f"Time {time}: Notes {', '.join(notes)}")
